641409be41276cddeda41ea8dca2328891e3a085
Skyler Lewis <sblnog@gmail.com>
2014-01-29 21:25:39 -0700

    Initial Commit

---
+++ DomainValidation/Domain.php
@@ -0,0 +1,23 @@
+<?php namespace DomainValidation;
+
+class Domain {
+
+	public function checkUrlStatus($url) {
+		$handle = curl_init($url);
+		curl_setopt($handle, CURLOPT_HEADER, true);
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($handle, CURLOPT_NOBODY, true);
+		curl_setopt($handle, CURLOPT_FRESH_CONNECT, true);
+		curl_setopt($handle, CURLOPT_TIMEOUT, 5);
+		curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($handle, CURLOPT_FAILONERROR, true);
+
+		$response = curl_exec($handle);
+		$httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+		curl_close($handle);
+		return $httpCode;
+	}
+
+
+}
